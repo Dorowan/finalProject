2504106ccad8c5897f0d87668085f9943aa76d7e
Dorowan <dbdorian@gmail.com>
2017-03-06 16:35:57 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
+$(document).ready(function(){
+  document.getElementById("move").innerHTML = "<p> Please fill in the blanks and click that beautiful button below: </p>"
+$("#calctime").click(function(e){
 
-document.getElementById("junk").innerHTML = "<p> Please fill in the blanks and click that beautiful button below: </p>"
+  e.preventDefault();
+
+  var leavinglocale = $("#departcity").val();
+  var showuplocale = $("#arrivalcity").val();
+
+    console.log(leavinglocale)
+    console.log(showuplocale)
+})
+
+  //fetch('')
+  //     .then(function() {
+  //       return response.json();
+  // })
+  // .then(function(data){
+  //   console.log(data)
+  //
+  // })
+  })
